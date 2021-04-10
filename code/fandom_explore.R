b29bb01c35b31849d0172e1416e266c5d659a4f0
Anderazu <ariadne.suspires@gmail.com>
2021-04-10 08:57:59 -0400

    Exploratory plots/counts

--- code/fandom_explore.R
+++ code/fandom_explore.R
@@ -1,19 +1,21 @@
-# Pull all rows for a specific fandom tag from works, convert to long data frame
+# Read in saved/tagged works info for a fandom and start exploring.
 
 
 library(tidyverse)
-library(ggplot2)
 
 
 # Import data
 (load("data/works_RWBY.Rda"))
-#(load("data/tags.Rda"))
+#(load("data/tags_RWBY.Rda"))
 
 
-# Exploratory looks
+## Exploratory looks
+
+# Tag type frequency
 wtagged %>% group_by(type) %>% 
   count()
 
+# Word count histogram
 wred %>% 
   ggplot(aes(x = word_count)) + 
   geom_histogram(bins = 40) +
@@ -21,9 +23,46 @@ wred %>%
   scale_x_log10() + 
   ggtitle("Word count frequency (RWBY)")
 
+ggsave("plots/wordcount_RWBY.png")
+
+# Language frequency
 wred %>% 
-  count(language)
+  count(language) %>% 
+  mutate(frac = n / nrow(wred))
   
-# Come back to add works / time plots
+
+## Work frequency over time
+
+# Cumulative work count
+works <- wred %>% 
+  mutate(ctAll = row_number()) 
+  
+# For daily totals, cut to unique creat_date and max value of ct
+ct_all <- works %>% 
+  group_by(creat_date) %>% 
+  summarize(ctAll = max(ctAll))
+
+# Plot all works over time
+ct_all %>% 
+  ggplot(aes(x = creat_date, y = ctAll)) +
+  geom_line() + 
+  ggtitle("Cumulative works over time (RWBY)")
+
+# How exponential is it?
+ct_all %>% 
+  ggplot(aes(x = creat_date, y = ctAll)) +
+  geom_line() + 
+  scale_y_log10() + 
+  ggtitle("Cumulative works over time (RWBY)")
 
 
+## Start checking out relationship tags
+
+wtagged %>% 
+  filter(type == "Relationship") %>% 
+  group_by(name) %>% 
+  count() %>% 
+  arrange(desc(n)) 
+
+wtagged %>% 
+  filter(type == "Relationship", name == "Redacted")
