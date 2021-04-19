bc019ea7fc27804c3729cfaddb669dc8b06ec5c1
Anderazu <ariadne.suspires@gmail.com>
2021-04-18 21:30:30 -0400

    Make network from tag-merged data frames

--- code/tag_network_test.R
+++ code/tag_network_test.R
@@ -9,6 +9,7 @@ library(igraph)
 (load("data/works_RWBY.Rda"))
 (load("data/tags_RWBY.Rda"))
 
+wtagged <- wtagged_fandom %>% ungroup()
 
 ## Exploratory looks
 
@@ -28,88 +29,95 @@ wtagged %>%
   count() %>% 
   arrange(desc(n)) 
 
-# Rarepairs? (romantic or otherwise, I think)
+# Rarepairs or otherwise uncommon Relationship tags
 wtagged %>% 
   filter(type == "Relationship", name == "Redacted")
 
 
 ## Bipartite network from works/tags
 
-# Before doing this, need to replace any duplicate tags with their merged info.
-
-# Preliminary edge data frame from tagged works list
+# Edge data frame from tagged works list
 df_rel <- wtagged %>% 
   filter(type == "Relationship") %>% 
   select(wid, tag_list, name, word_count)
 
-# Replace any tags with merger IDs:
-
-# 1. Subset edge data frame by whether rows need merging
-temp1 <- df_rel %>% 
-  left_join(tred %>% select(id, merger_id), by = c("tag_list" = "id")) %>% 
-  filter(is.na(merger_id)) %>%   # if merger_id = NA, no merge needed
-  select(-merger_id)
-
-temp2 <- df_rel %>% 
-  left_join(tred %>% select(id, merger_id), by = c("tag_list" = "id")) %>% 
-  filter(!is.na(merger_id))      # non-NA merger_id, need to merge
-
-# 2. For frame of duplicated tags, replace with merged ID and name
-temp2merge <- temp2 %>% 
-  select(-tag_list) %>%   
-  left_join(tred %>% select(id, name), 
-            by = c("merger_id" = "id")) %>%  # pull in merged names and tags
-  rename(tag_list = merger_id, name = name.y) %>%   # align names with df_rel
-  select(wid, tag_list, name, word_count)    # cut to merged info columns
-
-# 3. Recombine frames into new, merged edge list data
-df_rel2 <- bind_rows(temp1, temp2rev) %>% 
-  arrange(wid)
-
 
 # Build igraph object
 # 1. Modify wid column to avoid duplicate labels with tag numbers
-df_rel3 <- df_rel2 %>% 
+df_rel2 <- df_rel %>% 
   mutate(wid = paste0("W", as.character(wid))) %>% 
   mutate(tag_list = as.character(tag_list))
 
 # Before merging tags, gb_rel had 28843 nodes; after, it has 26384
-gb_rel <- df_rel3 %>% 
+gb_rel <- df_rel2 %>% 
   graph_from_data_frame()
 
+summary(gb_rel)
+
 # Set node type
-vertex_attr(gb_rel, "type") <- V(gb_rel)$name %in% pull(df_rel3, var = wid)
+vertex_attr(gb_rel, "type") <- V(gb_rel)$name %in% pull(df_rel2, var = wid)
 table(V(gb_rel)$type)
 
 # Bipartite projection
 bipartite_projection_size(gb_rel)
 
-# Before tag merger, g_rel had 7759 nodes; after, it has 5300
+# Before tag merger, g_rel had 7759 nodes; after, it has 5307
 ptm <- proc.time()
 g_rel <- bipartite_projection(gb_rel)[[1]]
 (proc.time() - ptm)   # about 30 seconds
 
 # May be easiest to match node attributes as data frame?
-df_grel <- g_rel %>% igraph::as_data_frame(what = "both")
+df_grel <- g_rel %>% 
+  igraph::as_data_frame(what = "both") %>% 
+  map(as_tibble)
 
-tagsumm <- df_rel3 %>% select(-wid) %>% 
+str(df_grel, max.level = 2)
+
+tagsumm <- df_rel2 %>% 
+  select(-wid) %>% 
   group_by(tag_list) %>% 
   summarize(name = unique(name), 
             word_count = sum(word_count, na.rm = TRUE)) %>% 
   arrange(as.numeric(tag_list)) 
 
-# PICK UP HERE
-# Not working yet
-df_reltags$vertices %>% 
-  slice_head(n = 5) %>% 
-  left_join(tagsumm, by = c("name" = "tag_list"))
+# Match in longer tag name and wordcount
+df_grel$vertices <- df_grel$vertices %>% 
+  left_join(tagsumm, by = c("name" = "tag_list")) %>% 
+  rename(name_long = name.y)
+
+g2 <- graph_from_data_frame(df_grel$edges, 
+                            vertices = df_grel$vertices)
+summary(g2)
 
 
- 
-# Match in tag labels (not working yet, matches in whole vector)
-vertex_attr(g_rel, "label") <- df_rel[match(V(g_rel)$name, df_rel$tag_list), 
-                                      "name"]
+## Check out the igraph object
 
+# Degree distribution
+degg2 <- tibble(name = V(g2)$name, deg = degree(g2)) 
+
+degg2 %>% group_by(deg) %>% count()
+
+degg2 %>% ggplot(aes(x = deg)) + 
+  geom_histogram(bins = 40) + 
+  scale_y_log10() #+
+  #scale_x_log10()
+
+# Look at isolates
+degg2 %>% filter(deg == 0) %>% 
+  left_join(df_grel$vertices)
+
+# Remove isolates
+g3 <- delete_vertices(g2, which(degree(g2) == 0)) 
+
+# Plot that shit
 ptm <- proc.time()
-plot(g_rel, vertex.size = 7, vertex.label = V(g_rel)$label)
-(proc.time() - ptm)   # 
+plot(g3, vertex.size = 7, vertex.label = NA)
+(proc.time() - ptm)   # about 20 seconds
+
+
+# Save a copy
+vertex_attr(g3, "Label") <- V(g3)$name_long
+write_graph(g3, file = "data/nw_relationship_RWBY.graphml", 
+            format = "graphml")
+write_graph(g3, file = "data/nw_relationship_RWBY.gml", 
+            format = "gml")
