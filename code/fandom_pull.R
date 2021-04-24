7253b8e95e5d8777d567a0c7eef521fec1cb153b
Anderazu <ariadne.suspires@gmail.com>
2021-04-24 14:33:45 -0400

    Starting to look at other fandoms

--- code/fandom_pull.R
+++ code/fandom_pull.R
@@ -1,6 +1,7 @@
 # Pull all rows for a specific fandom tag from works, convert to long data frame
 
 # 18 Apr 2021: There's a single RWBY-used tag that still has a merger id. Why?
+# 24 Apr 2021: Fixed, see merged_works_debug.R or merge_tags.Rmd for details.
 
 
 library(tidyverse)
@@ -22,13 +23,18 @@ rm(works_merged)
 
 # Regex to match: Start of line OR non-digit, tag, non-digit OR end of line
 makePat <- function(x) {
-  pat <- paste0("(^|[^[:digit:]])", x, "([^[:digit:]]|$)") 
+  pat <- paste0("(^|\\+)", x, "(\\+|$)") 
 }
 
 # Find tag number for a given name
+# Need to update, only works for exact matches
 getTagID <- function(x, df = tags) {
   df %>% filter(name == x) %>% pull(var = id)
 }
+
+# If you don't know the exact name, will need to look for it
+tags %>%
+  filter(str_detect(name, pattern = "Portia"))
 
 
 ## Pick a fandom and filter works for those entries
@@ -38,17 +44,28 @@ getTagID <- function(x, df = tags) {
 # Filter to works rows of interest
 tagpat <- makePat(getTagID("RWBY"))
 
+#tags %>% 
+#  filter(type == "Fandom") %>% 
+#  filter(str_detect(name, pattern = "Stardew"))
+
+#tagpat <- makePat(21232893)  # My Time at Portia
+#tagpat <- makePat(8713639)  # Stardew Valley
+
+ptm <- proc.time()
 wred <- works %>% 
   filter(grepl(pattern = tagpat, x = tags))
+(proc.time() - ptm)   
 
 
 # Convert tags values to list-column, then unnest into long data frame
+ptm <- proc.time()
 wlong_fandom <- wred %>% 
   mutate(tag_list = stringr::str_split(.data$tags, 
                                        stringr::fixed("+"))) %>% 
   unnest(tag_list) %>% 
   mutate(tag_list = as.integer(tag_list)) %>% 
   select(-tags)
+(proc.time() - ptm)   
 
 # 2. Add tag names to long data frame
 wtagged_fandom <- wlong_fandom %>% 
@@ -68,6 +85,7 @@ wtagged_fandom %>%
 
 # Example
 tags %>% filter(id > 54605320)
+#tags %>% filter(id > 43613120)
 
 
 ## Filter tags to in-use list
@@ -84,7 +102,7 @@ tred <- tags_used %>%
   left_join(tags, by = c("tag_list" = "id")) %>% 
   rename(id = tag_list)
 
-tred %>% filter(!is.na(merger_id))  # Why is there still a non-merged tag here?
+tred %>% filter(!is.na(merger_id))  # Should be empty if tags merged correctly
 
 
 # Save filtered data frames
