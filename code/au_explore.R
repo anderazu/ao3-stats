9f80cb589b4e41ddfa873ef7353aa7ec1c32504f
Anderazu <ariadne.suspires@gmail.com>
2021-08-28 17:03:08 -0400

    Pull AU frequencies, wordcloud not working

--- code/au_explore.R
+++ code/au_explore.R
@@ -22,6 +22,11 @@ autags <- freetags %>%
   filter(str_detect(name, pattern = temppat[1]) | 
          str_detect(name, pattern = temppat[2]))
 
+# Trying to do better job of eliminating false positives
+#tempau <- freetags %>% 
+#  filter(str_detect(name, pattern = temppat[1]) | 
+#           str_detect(name, pattern = "AU\b"))
+
 # Filter works frame to rows with one of those tags
 auworks <- wtagged %>% 
   filter(tag_list %in% autags$id) %>% 
@@ -30,4 +35,24 @@ auworks <- wtagged %>%
 
 wkau <- wtagged %>% filter(wid %in% pull(auworks))
 
+
+## Plot wordcloud of AU tags
+
+# Store frequency of tags
+aufreq <- wtagged %>% 
+  filter(tag_list %in% autags$id) %>% 
+  count(name, sort = TRUE) 
+
+# Remove particular rows that I don't want
+aufreq <- aufreq %>% 
+  filter(!str_detect(name, "SLEEP"))
+
+# Remove the AU part of the label
+aufreq$shortname <- aufreq %>% 
+  pull(name) %>% 
+  gsub("[[:space:]]+AU$", "", x = .) %>% 
+  gsub("Alternate Universe - ", replacement = "", x = .)
+
+# This hangs, need to fix it
+wordcloud(aufreq$shortname, aured$n, min.freq = 2)
 
